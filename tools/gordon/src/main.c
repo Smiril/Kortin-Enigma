d20cf922cd4fbc3d2e0b2f9940cfa74e6558b2f2
Smiril <Smiril@users.noreply.github.com>
2023-01-13 19:34:14 +0100

    Update main.c

--- tools/gordon/src/main.c
+++ tools/gordon/src/main.c
@@ -8,117 +8,68 @@
 * License.....: MIT
 ******************************************************************************/
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <stdint.h>
-
 #include <stdbool.h>
-
 #include <time.h>
-
 #include <math.h>
-
 #include <unistd.h>
-
 #include <string.h>
-
 #include <ctype.h>
-
 #include <memory.h>
-
 #include <iconv.h>
 
 #define CA_PRIVATE_IMPLEMENTATION
 
-static const char *coin(int fex) {
-
+static void *coin(int fex)
+{
     bool isDuplicate = false;
-
     //---------------------------------------------------------------------
-
     const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    int max_index = (sizeof(charset) - 1);
-
+    int max_index = (strlen(charset) - 1);
     //---------------------------------------------------------------------
-
-    const char *picked = NULL;
-
-    const char *number = NULL;
-
+    char picked[fex];
+    memset( picked, 0, fex );
+    char number[1];
     int j = 0,i = 0;
 
-    for (i = 0; i < fex; i++)
-    {
-
-        do
-        {
-
+    for (i = 0; i < fex; i++) {
+        do {
             int target = rand() % max_index; // Generate
-
-            strncpy((char *)number,&charset[target],1); // Pick
-
+            strncpy((char *)number,&charset[target],strlen(number)); // Pick
             // Check for duplicates
-
-            for (j = 0; j < fex; j++)
-            {
-
-                if (number == &picked[j])
-                {
-
+            for (j = 0; j < fex; j++) {
+                if (number == &picked[j]) {
                     isDuplicate = true;
-
                     break; // Duplicate detected
-
                 } // end if
                 else {
-
-                    memmove((void *)&charset[max_index], &charset[max_index], atoi(&charset[max_index]) - atoi(number));
-
-                    max_index--;
-
-                    isDuplicate = false;
-
-                    break; // No Duplicate detected
-
+                    //memmove((void *)&charset[max_index], &charset[max_index], max_index - strlen(number));
+                    //max_index--;
+                    strncpy((char *)&picked[j],number,strlen(number)); // picked
+                    //isDuplicate = false;
+                    // break; // No Duplicate detected
                 } // end else
-
             } // end for
-
         } // end do
         while (isDuplicate); // equivalent to while(isDuplicate == true)
-
-        if (!isDuplicate) { // equivalent to if(isDuplicate == false)
-
-            strncpy((char *)&picked[j],number,1); // picked
-
-        } // end if
-
     } // end for
-
-    return picked;
-
+    printf("%s\n",picked);
+   
+    return 0;
 }
 
-int main(void) {
-
+int main(void)
+{
+    int i = 0;
     time_t start, stop;
-
     start = time(NULL);
-
-    for (int i = 0; i < 6; i++) {
-
-        printf("%s\n", coin(26));
-
+    for (i = 0; i < 6; i++) {
+        coin(26);
     } // end for
-
-    printf("%s\n",coin(5));
-
+    coin(5);
     stop = time(NULL);
-
     printf("Time elapsed : %ld seconds\n",(stop - start));
 
     return 0;
-
 }
