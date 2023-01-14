ca391d0cec942f61112d160352a7682c188a3765
Smiril <Smiril@users.noreply.github.com>
2023-01-14 09:58:01 +0100

    Update main.c

--- tools/gordon/src/main.c
+++ tools/gordon/src/main.c
@@ -25,7 +25,7 @@ static char coin(int fex)
 {
     bool isDuplicate = false;
     //---------------------------------------------------------------------
-    char charset[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char charset[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     //int max_index = (strlen(charset));
     int max_index = (sizeof(charset) - 1);
     //---------------------------------------------------------------------
@@ -33,31 +33,31 @@ static char coin(int fex)
     char picked[fex];
     //char number[1];
     char newabc[1];
-    int j = 0,i = 0,k = 0;
-
+    int j = 0,i = 0;
+    
     for (i = 0; i <= fex; i++) {
         memset( picked, 0, fex );
         do {
-            for (k = 0 ; k <= 1; k++) {
-                newabc[k] = charset[rand() % max_index];
-            } // end for
+            newabc[0] = charset[rand() % max_index];
             // Check for duplicates
             for (j = 0; j <= fex; j++) {
-                if (&newabc[k] == &picked[j]) {
-                        isDuplicate = true;
-                        break; // Duplicate detected
+                if (&newabc[1] == &picked[j]) {
+                    isDuplicate = true;
+                    break; // Duplicate detected
                 } // end if
                 else {
-                    memmove((void *)&charset[max_index],&charset[max_index],max_index - strlen(&newabc[k]));
-                    isDuplicate = false;
+                    strncpy((char *)&picked[j],&newabc[0],strlen(&newabc[0])); // picked
+                    printf("%c",picked[j]);
+                    memmove((void *)&charset[max_index],&charset[max_index],max_index - 1);
+                    break;
+                    //isDuplicate = false;
                 } // end else
             } // end for
         } // end do
         while (isDuplicate); // equivalent to while(isDuplicate == true)
         
-        strncpy((char *)&picked[j],&newabc[k],strlen(&newabc[k])); // picked
-        printf("%c",picked[j]);
-        
+        //strncpy((char *)&picked[j],&newabc[1],strlen(&newabc[1])); // picked
+        //printf("%c",picked[j]);
     } // end for
     printf("\n");
     
