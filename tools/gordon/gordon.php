5e29373fc1de6e2bbafff5088c653d4e05d4ace0
Smiril <Smiril@users.noreply.github.com>
2023-01-15 19:14:56 +0100

    Update gordon.php

--- tools/gordon/gordon.php
+++ tools/gordon/gordon.php
@@ -1,5 +1,10 @@
 <?php
-echo "<style type=\"text/css\"> body { background-color: #336699; text-color: #ffffff; } </style>";
+echo "<style type=\"text/css\">
+body {
+    background-color: #336699;
+    text-color: #ffffff;
+}
+</style>";
 
 function rotor($arr) {
     foreach ($arr as $v) {
