9cecde0efc87a53849b9e2b4c768e196dd46d245
Smiril <Smiril@users.noreply.github.com>
2023-01-20 14:17:38 +0100

    Update main.c

--- enigma_v4/main.c
+++ enigma_v4/main.c
@@ -32,6 +32,7 @@ void configmain(main_ctx_t *main_ctx,char *hugh) {
     fp = fopen(hugh,"r");
     struct stat st;
     fstat(fileno(fp), &st);
+    size_t size = st.st_size;
     if (433 != size) {
         printf("config file is corrupt\n");
         exit(1);
