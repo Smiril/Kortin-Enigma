4e048bab96c6927e9cbf49276208dd0db1733995
Smiril <Smiril@users.noreply.github.com>
2023-01-24 21:39:15 +0100

    Update main.c

--- enigma_v4/main.c
+++ enigma_v4/main.c
@@ -78,6 +78,54 @@ void configmain(main_ctx_t *main_ctx,char *hugh) {
     xmlFreeDoc(doc);
 }
 
+#ifdef __WIN32__
+int inet_pton(int af, const char *src, void *dst)
+{
+  struct sockaddr_storage ss;
+  int size = sizeof(ss);
+  char src_copy[INET6_ADDRSTRLEN+1];
+
+  ZeroMemory(&ss, sizeof(ss));
+  /* stupid non-const API */
+  strncpy (src_copy, src, INET6_ADDRSTRLEN+1);
+  src_copy[INET6_ADDRSTRLEN] = 0;
+
+  if (WSAStringToAddress(src_copy, af, NULL, (struct sockaddr *)&ss, &size) == 0) {
+    switch(af) {
+      case AF_INET:
+    *(struct in_addr *)dst = ((struct sockaddr_in *)&ss)->sin_addr;
+        break;
+      case AF_INET6:
+    *(struct in6_addr *)dst = ((struct sockaddr_in6 *)&ss)->sin6_addr;
+        break;
+    }
+  }
+  return 0;
+}
+
+const char *inet_ntop(int af, const void *src, char *dst, socklen_t size)
+{
+  struct sockaddr_storage ss;
+  unsigned long s = size;
+
+  ZeroMemory(&ss, sizeof(ss));
+  ss.ss_family = af;
+
+  switch(af) {
+    case AF_INET:
+      ((struct sockaddr_in *)&ss)->sin_addr = *(struct in_addr *)src;
+      break;
+    case AF_INET6:
+      ((struct sockaddr_in6 *)&ss)->sin6_addr = *(struct in6_addr *)src;
+      break;
+    default:
+      return AF_INET;
+  }
+  /* cannot direclty use &size because of strict aliasing rules */
+  return (WSAAddressToString((struct sockaddr *)&ss, sizeof(ss), NULL, dst & s) == 0)?dst : NULL;
+}
+#endif
+
 int valid_digit(char *ip_str)
 {
     while (*ip_str) {
@@ -127,7 +175,21 @@ int is_valid_ip(char *ip_str)
 }
 
 int create_tcp_socket(){
-    int socket_desc;
+#ifdef __WIN32__
+    WSADATA wsa;
+    SOCKET socket_desc;
+ 
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+    {
+        printf("Failed. Error Code : %d\n",WSAGetLastError());
+        return 1;
+    }
+     
+    printf("Initialised.\n]");
+#else
+  int socket_desc;
+#endif
     
   if((socket_desc = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
     perror("Can't create TCP socket");
@@ -137,7 +199,21 @@ int create_tcp_socket(){
 }
 
 int create_udp_socket(){
-    int socket_desc;
+#ifdef __WIN32__
+    WSADATA wsa;
+    SOCKET socket_desc;
+ 
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+    {
+        printf("Failed. Error Code : %d\n",WSAGetLastError());
+        return 1;
+    }
+     
+    printf("Initialised.\n]");
+#else
+  int socket_desc;
+#endif
 
   if((socket_desc = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
     perror("Can't create UDP socket");
@@ -150,7 +226,21 @@ int create_udp_socket(){
 //sysctl -w net.ipv4.ping_group_range="0 0"
 
 int create_icmp_socket(){
-    int socket_desc;
+#ifdef __WIN32__
+    WSADATA wsa;
+    SOCKET socket_desc;
+ 
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+    {
+        printf("Failed. Error Code : %d\n",WSAGetLastError());
+        return 1;
+    }
+     
+    printf("Initialised.\n]");
+#else
+  int socket_desc;
+#endif
 
   if((socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP)) < 0){
     perror("Can't create ICMP socket");
@@ -160,7 +250,21 @@ int create_icmp_socket(){
 }
 
 int create_igmp_socket(){
-    int socket_desc;
+#ifdef __WIN32__
+    WSADATA wsa;
+    SOCKET socket_desc;
+ 
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+    {
+        printf("Failed. Error Code : %d\n",WSAGetLastError());
+        return 1;
+    }
+     
+    printf("Initialised.\n]");
+#else
+  int socket_desc;
+#endif
 
   if((socket_desc = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP)) < 0){
     perror("Can't create IGMP socket");
@@ -170,7 +274,21 @@ int create_igmp_socket(){
 }
 
 int create_raw_socket(){
-    int socket_desc;
+#ifdef __WIN32__
+    WSADATA wsa;
+    SOCKET socket_desc;
+ 
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+    {
+        printf("Failed. Error Code : %d\n",WSAGetLastError());
+        return 1;
+    }
+     
+    printf("Initialised.\n]");
+#else
+  int socket_desc;
+#endif
 
   if((socket_desc = socket(AF_INET, SOCK_STREAM, IPPROTO_RAW)) < 0){
     perror("Can't create RAW socket");
