5b59bda0e9b5e964dc4991ad12162654d995290e
Smiril <Smiril@users.noreply.github.com>
2023-01-26 19:49:08 +0100

    Update main.c

--- enigma_v4/main.c
+++ enigma_v4/main.c
@@ -55,9 +55,8 @@ void configmain(main_ctx_t *main_ctx,char *hugh) {
         //if(cur->type == XML_ELEMENT_NODE) {
             if (xmlStrcmp(cur->name, (const xmlChar *) config[count++])) {
                 if((uri =  xmlGetProp(cur,(xmlChar *)"name")) != NULL) {
-                    strcpy(&main_ctx->rotor[count1][MSGC],(const char *)uri);
+                    strcpy(&main_ctx->rotor[count1++][MSGC],(const char *)uri);
                     xmlFree(uri);
-                    count1++;
                 }
             }
             else if (xmlStrcmp(cur->name, (const xmlChar *) "xmlref")) {
