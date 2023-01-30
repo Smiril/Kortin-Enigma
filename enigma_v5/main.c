bf58dd7aa76c388737845aa588faeb01de669281
Smiril <Smiril@users.noreply.github.com>
2023-01-30 20:02:49 +0100

    Update main.c

--- enigma_v5/main.c
+++ enigma_v5/main.c
@@ -187,7 +187,6 @@ static int ip_version(const char *src) {
 
     ret = getaddrinfo(src, NULL, &hint, &res);
     if (ret) {
-        printf("Invalid address");
         perror("addr");
         return 1;
     }
