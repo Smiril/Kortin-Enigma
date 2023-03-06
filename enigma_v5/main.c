d90130d7cbda5d351eece02749f6852597b9c1d1
Smiril <Smiril@users.noreply.github.com>
2023-03-06 09:02:59 +0100

    Update main.c

--- enigma_v5/main.c
+++ enigma_v5/main.c
@@ -2263,9 +2263,9 @@ void sbfParams(main_ctx_t *main_ctx)
     
     for (int i = 0;i < core;i++) {
         //pthread_t tid = malloc(1 * sizeof(pthread_t));
-        pthread_create(*(pthread_t**)&tid[i], NULL, reader, (void*)&fds[i]);
-        pthread_create(*(pthread_t**)&tid[i], NULL, permuteOX, (void*)&fds[i]);
-        printf("created: %llu\n", (unsigned long long)&tid[i]);
+        pthread_create(*(pthread_t**)&tid, NULL, reader, (void*)&fds[i]);
+        pthread_create(*(pthread_t**)&tid, NULL, permuteOX, (void*)&fds[i]);
+        printf("created: %llu\n", (unsigned long long)&tid);
         //read(fds[0], &tid[i], sizeof(tid[i]));
         //write(fds[1], &tid[i], sizeof(tid[i]));
         //printf("joining: %llu\n", (unsigned long long)&tid[i]);
@@ -2385,9 +2385,9 @@ void bfParams(main_ctx_t *main_ctx)
     
     for (int i = 0;i < core;i++) {
         //pthread_t tid = malloc(1 * sizeof(pthread_t));
-        pthread_create(*(pthread_t**)&tid[i], NULL, reader, (void*)&fds[i]);
-        pthread_create(*(pthread_t**)&tid[i], NULL, permuteAX, (void*)&fds[i]);
-        printf("created: %llu\n", (unsigned long long)&tid[i]);
+        pthread_create(*(pthread_t**)&tid, NULL, reader, (void*)&fds[i]);
+        pthread_create(*(pthread_t**)&tid, NULL, permuteAX, (void*)&fds[i]);
+        printf("created: %llu\n", (unsigned long long)&tid);
         //read(fds[0], &tid[i], sizeof(tid[i]));
         //write(fds[1], &tid[i], sizeof(tid[i]));
         //printf("joining: %llu\n", (unsigned long long)&tid[i]);
